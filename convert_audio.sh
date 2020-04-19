328eaa5df027be127e50b294282556a7fdeed5ef
Betsy <betsysneller@gmail.com>
2020-04-19 15:37:43 -0400

    Updated convert_audio.sh

--- convert_audio.sh
+++ convert_audio.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+export GOOGLE_APPLICATION_CREDENTIALS="/Users/betsysneller/Desktop/mi-covid-diaries/mi covid diaries-31ac6d932f0d.json"
+
 echo "Begin program: "
+echo "==============="
 input="$1"
 pathtofile=`dirname "$input"`
 filename=`basename "$input"`
@@ -9,12 +12,18 @@ fname="${filename%.*}"
 #echo "${fname}"
 fname="${fname// /_}.wav"
 
+echo "Coverting file to .wav"
+echo "==============="
 # convert file to .wav
 ffmpeg -i "${input}" "${fname}"
+
+echo "Uploading to google cloud bucket"
+echo "==============="
 
 # copy file to gcloud storage
 gsutil cp ${fname} gs://mi-covid_diaries-files/
 echo "Successfully completed converting and uploading to google cloud. Converted file is ${fname}"
+echo "==============="
 
 line=$(file ${fname})
 a=( $line )
@@ -24,9 +33,12 @@ bitrate=${a[${#a[@]}-2]}
 echo $bitrate
 
 echo "Transcribing file"
+echo "==============="
 python3 google_cloud_speech_transcriber.py gs://mi-covid_diaries-files/"${fname}" $bitrate
 
 
 gsutil rm gs://mi-covid_diaries-files/${fname}
 
 echo "Transcription complete and file removed from google cloud bucket"
+echo "==============="
+echo "Done!"
