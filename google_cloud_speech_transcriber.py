14c8b0b69b57dea4d9a9f0a794e8bd78b3e7f157
Matt Churgin <mattchurgin@gmail.com>
2020-04-25 15:45:43 -0400

    added tracking of word time stamps

--- google_cloud_speech_transcriber.py
+++ google_cloud_speech_transcriber.py
@@ -4,6 +4,7 @@ from google.cloud.speech_v1 import enums
 import io
 import sys
 import os
+import pickle
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/betsysneller/Desktop/mi-covid-diaries/mi covid diaries-31ac6d932f0d.json"
 #os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/mattchurgin/Desktop/mi-covid-diaries/mi covid diaries-31ac6d932f0d.json"
@@ -16,10 +17,10 @@ def sample_long_running_recognize(storage_uri):
       local_file_path Path to local audio file, e.g. /path/audio.wav
     """
     # standard speech client
-    #client = speech_v1.SpeechClient()
+    client = speech_v1.SpeechClient()
 
     # if utilizing speaker diarization
-    client = speech_v1p1beta1.SpeechClient()
+    #client = speech_v1p1beta1.SpeechClient()
 
     # local_file_path = 'resources/brooklyn_bridge.raw'
 
@@ -46,7 +47,8 @@ def sample_long_running_recognize(storage_uri):
         "encoding": encoding,
         "enable_automatic_punctuation": True,
         "enable_speaker_diarization": True,
-        "diarization_speaker_count": diarization_speaker_count,
+        #"diarization_speaker_count": diarization_speaker_count,
+        "enable_word_time_offsets": True
     }
     audio = {"uri": storage_uri}
 
@@ -55,8 +57,9 @@ def sample_long_running_recognize(storage_uri):
     print(u"Waiting for operation to complete...")
     response = operation.result()
     outtext = list()
-    out_text_speaker = list()
-    out_text_speaker_label = list()
+    out_word_tmstmps = list()
+    #out_text_speaker = list()
+    #out_text_speaker_label = list()
     for result in response.results:
         # First alternative is the most probable result
         alternative = result.alternatives[0]
@@ -64,15 +67,17 @@ def sample_long_running_recognize(storage_uri):
         outtext.append(alternative.transcript)
 
         for word in alternative.words:
-            print(u"Speaker: {}, Word: {}".format(word.speaker_tag, word.word))
-            out_text_speaker.append(word.word)
-            out_text_speaker_label.append(word.speaker_tag)
+            #print(u"Speaker: {}, Word: {}".format(word.speaker_tag, word.word))
+            #out_text_speaker.append(word.word)
+            #out_text_speaker_label.append(word.speaker_tag)
+            out_word_tmstmps.append((word.word, word.start_time.seconds, word.start_time.nanos,
+                             word.end_time.seconds, word.end_time.nanos))
 
-    return outtext, out_text_speaker, out_text_speaker_label
+    return outtext, out_word_tmstmps
 
 audio_file = sys.argv[1]
 print(f'Transcribing file: {audio_file}:')
-textout, textout_speaker, textout_speaker_label = sample_long_running_recognize(audio_file)
+textout, timestamps = sample_long_running_recognize(audio_file)
 
 outfilename = os.path.split(audio_file)[-1]
 outpath = sys.argv[3]
@@ -84,8 +89,5 @@ textfinal = ".\n".join(textsplit)
 
 with open(outpathname, "w") as text_file:
     text_file.write(textfinal)
-#with open("transcript_speaker.txt", "w") as text_file:
-#    text_file.write(" ".join(textout_speaker))
-#with open("transcript_speaker_label.txt", "w") as text_file:
-#    text_file.write(" ".join(str(textout_speaker_label)))
-
+with open('word_tmstmps.pkl', 'wb') as f:
+    pickle.dump(timestamps, f)
